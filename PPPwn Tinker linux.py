cc1b9a3ed17c456bf99b24071a4adc304b357b19
DrYenyen <hristiyan212121@gmail.com>
2024-10-16 00:41:28 +0100

    Update PPPwn Tinker linux.py

--- PPPwn Tinker linux.py
+++ PPPwn Tinker linux.py
@@ -116,11 +116,15 @@ def run_command():
                 terminal_type = "konsole"
             elif os.path.exists("/usr/bin/gnome-terminal") or os.path.exists("/usr/local/bin/gnome-terminal"):
                 terminal_type = "gnome"
+            elif os.path.exists("/usr/bin/xfce4-terminal") or os.path.exists("/usr/local/bin/xfce4-terminal"):
+                terminal_type = "xfce4"
                 
-            if terminal_type == "gnome":
+            if terminal_type == "gnome":  
                 subprocess.Popen(['gnome-terminal', '--working-directory', current_directory, '--', 'bash', '-c', command + '; exec bash'])
             elif terminal_type == "konsole":
                 subprocess.Popen(['konsole', '-e', command])
+            elif terminal_type == "xfce4":
+                    subprocess.Popen(['xfce4-terminal', '--hold', '-e', command])
             else:
                 print("No supported terminal found.")
 
